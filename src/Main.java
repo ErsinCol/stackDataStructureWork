36afe0f6cdf11a856088d3ba8fe4af4e2e2be39e
ERSİN ÇÖL <ersincol235@gmail.com>
2022-05-01 18:33:46 +0300

    work1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Stack stack=new Stack();
+        if(stack.empty())
+        {
+            System.out.println("Yapımız boş...");
+        }
+        stack.push("İstanbul");
+        stack.push("İzmir");
+        stack.push("Ankara");
+        stack.push("Rize");
+        System.out.println(stack);
+        System.out.println(stack.search("Rize"));
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack);
     }
 }
